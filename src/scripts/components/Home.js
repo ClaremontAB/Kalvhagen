3100bf21bdef00b0dfbbf44561dbe20fe6b7f1ce
Linda Andersson <linda.andersson@claremont.se>
2017-09-18 17:06:03 +0200

    Test 2

--- src/scripts/components/Home.js
+++ src/scripts/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component {
                 <h1>Home</h1>
                 <p>Initial template project to show how we can fetch data, for now pages and posts from wordpress in a react project.
                     The data will be whatever you add in your wordpress installation. Try it out! :) 
-                    Testing Jenkins</p>
+                    Testing Jenkins 2</p>
             </div>
         );
     }
