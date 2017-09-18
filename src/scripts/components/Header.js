124fc58dcbb603ddef478f3fcee8198c558da9b9
Emelie Karlsson <emelie@emelies-mbp.claremont.un.dr.dgcsystems.net>
2017-09-18 16:48:50 +0200

    Just some formatting

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -3,19 +3,16 @@ import DataStore from 'flux/stores/DataStore.js'
 import '../../css/header.css'
 
 class Header extends React.Component {
-
-    render() {
-        return (
-
-
-            <div className="header">
-                <img src="../../../img/topLogo copy.png" alt="LOGOGOGO" className="topLogo"></img>
-                <Link to="/"/* style={{marginRight: '10px'}}*/ className="headerItem" >Home</Link>
-                <Link to="/posts" key="1" /*style={{marginRight: '10px'}}*/ className="headerItem" >Posts</Link>
-                <Link to="/pages" key="2" /*style={{marginRight: '10px'}}*/ className="headerItem" >Pages</Link>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="header">
+        <img src="../../../img/topLogo copy.png" alt="LOGOGOGO" className="topLogo"></img>
+        <Link to="/"/* style={{marginRight: '10px'}}*/ className="headerItem" >Home</Link>
+        <Link to="/posts" key="1" /*style={{marginRight: '10px'}}*/ className="headerItem" >Posts</Link>
+        <Link to="/pages" key="2" /*style={{marginRight: '10px'}}*/ className="headerItem" >Pages</Link>
+      </div>
+    );
+  }
 }
 
 export default Header;
