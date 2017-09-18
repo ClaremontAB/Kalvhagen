cedc3e875b0d16ccd0577b973357a03462de719a
Linda Andersson <linda.andersson@claremont.se>
2017-09-18 16:37:42 +0200

    Testing jenkins build hook

--- src/scripts/components/Home.js
+++ src/scripts/components/Home.js
@@ -6,7 +6,8 @@ class Home extends React.Component {
             <div>
                 <h1>Home</h1>
                 <p>Initial template project to show how we can fetch data, for now pages and posts from wordpress in a react project.
-                    The data will be whatever you add in your wordpress installation. Try it out! :) </p>
+                    The data will be whatever you add in your wordpress installation. Try it out! :) 
+                    Testing Jenkins</p>
             </div>
         );
     }
