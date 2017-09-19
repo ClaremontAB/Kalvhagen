9912592c5f980646dc427d37202ed6827958a8cc
Linda Andersson <linda.andersson@claremont.se>
2017-09-19 16:44:50 +0200

    Failing test again

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -3,7 +3,7 @@ const sum = require('../scripts/sum');
 describe('addition', () => {
   describe('positive numbers', () => {
     it('should add up', () => {
-      expect(1 + 2).toBe(3);
+      expect(1 + 3).toBe(3);
     });
   });
 });
