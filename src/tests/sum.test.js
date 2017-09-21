9d3a560d6d683249edab34351f6692768a9a55e1
Linda Andersson <linda.andersson@claremont.se>
2017-09-21 11:59:54 +0200

    Testing

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -3,7 +3,7 @@ const sum = require('../scripts/sum');
 describe('addition', () => {
   describe('positive numbers', () => {
     it('should add up', () => {
-      expect(sum(1, 2)).toBe(3);
+      expect(sum(1, 1)).toBe(3);
     });
   });
 });
