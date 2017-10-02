97dcbbdfea6b11331cdc1f9f0176b65fa45a309e
magnusharlin <31917628+magnusharlin@users.noreply.github.com>
2017-10-02 10:09:03 +0200

    Full code replacement
    * Replaced all code with code based on create-react-app, should work pretty much as previously, added a carousel component

---
+++ src/Posts.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import ReqHandler from './ReqHandler.js';
+import './css/page.css';
+
+class Posts extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    //debugger;
+    ReqHandler.getPosts()
+      .then(res => {
+        const posts = res;
+        this.setState({posts});
+      });
+  }
+    render() {
+      return (
+          <div className='page'>
+              <h1>Posts</h1>
+              <p>This page fetches and lists all posts from wordpress</p>
+              <ul>
+                {this.state.posts.map((post) =>
+                  <div key={post.id}>
+                    <h2> {post.title.rendered} </h2>
+                    <div dangerouslySetInnerHTML={{__html: post.content.rendered}} />
+                  </div>
+                )}
+              </ul>
+          </div>
+      );
+    }
+}
+
+export default Posts;
