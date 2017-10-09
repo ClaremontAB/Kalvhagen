b947ca0acae602cefd641bfac7181cd65858237a
magnusharlin <magnusharlin>
2017-10-09 10:35:49 +0200

    rearranged some elements and removed soem hidious color choices...

--- src/Home.js
+++ src/Home.js
@@ -7,8 +7,9 @@ class Home extends Component {
         return (
             <div>
                 <Carousel/>
+                <h1>Home</h1>
                 <div className="page">
-                  <h1>Home</h1>
+                  <p />
                   <p>Initial template project to show how we can fetch data, for now pages and posts from wordpress in a react project.
                       The data will be whatever you add in your wordpress installation. Try it out! :)
                       Testing Jenkins</p>
