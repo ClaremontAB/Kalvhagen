d4055fc532a09164b91bb2ab152d500ccf3da65e
magnusharlin <magnusharlin>
2017-10-09 09:48:32 +0200

    altered some css

--- src/Pages.js
+++ src/Pages.js
@@ -23,17 +23,20 @@ class Pages extends Component {
     render() {
 
         return (
-            <div className="page">
+            <div>
                 <h1>Pages</h1>
-                <p>This page fetches and lists all pages from wordpress</p>
-                <ul>
-                  {this.state.pages.map((page) =>
-                    <div key={page.id}>
-                      <h2> {page.title.rendered} </h2>
-                      <div dangerouslySetInnerHTML={{__html: page.content.rendered}} />
-                    </div>
-                  )}
-                </ul>
+                <div className="page">
+                  <p/>
+                  <p>This page fetches and lists all pages from wordpress</p>
+                  <ul>
+                    {this.state.pages.map((page) =>
+                      <div key={page.id}>
+                        <h2> {page.title.rendered} </h2>
+                        <div dangerouslySetInnerHTML={{__html: page.content.rendered}} />
+                      </div>
+                    )}
+                  </ul>
+                </div>
             </div>
         );
     }
