a1a02ea0682098f4c8bb4c151fcb07be63c8974c
magnusharlin <magnusharlin>
2017-10-30 11:30:17 +0100

    initial try to get a react router in place and working

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+
+class Home extends Component {
+
+  render () {
+    return (
+      <div> Hello home </div>
+    );
+  }
+}
+
+class App extends Component {
+
+	render () {
+		return (
+			<div>
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+            <li><Link to="/">Homes</Link></li>
+            <li><Link to="/category">Category</Link></li>
+            <li><Link to="/products">Products</Link></li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/page1" component={Home}/>
+          <Route path="/page2" component={Home}/>
+        </Switch>
+      </div>
+		);
+	}
+}
+
+export default App;
