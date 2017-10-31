d349be4d34106d4d1762976d812b98bd3f4bf0b7
Emelie Karlsson <emelie-sofie@users.noreply.github.com>
2017-10-31 11:01:37 +0100

    -

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import image from '../../resources/14774102-4eZZM.jpg';
 
 class Header extends Component {
 
   render() {
     return (
       <div className="header">
-        <img id="header-logo" src={image}></img>
+        <img id="header-logo" src={image} />
         <nav className="navbar navbar-light">
           <ul className="nav navbar-nav">
             <li className="nav-list"><Link to="/">Home</Link></li>
