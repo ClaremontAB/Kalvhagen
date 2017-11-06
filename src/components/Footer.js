2778f7cc5c55c40bfe07d92359f2fa815fa5a531
magnusharlin <magnusharlin>
2017-11-06 13:53:57 +0100

    added social media icons and links to the different pages, in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import facebookIcon from '../../resources/facebook.svg';
+import instagramIcon from '../../resources/instagram.svg';
+import youtubeIcon from '../../resources/youtube.svg';
 
 class Footer extends Component {
 
   render () {
     return (
       <div className="footer">
+        <div className="social-media-icons">
+          <a href="http://sv-se.facebook.com/pages/Kalvhagen-Sthlm/179548442086280" target="_blank">
+            <img src={facebookIcon} />
+          </a>
+          <a href="http://instagram.com/kalvhagensthlm" target="_blank">
+            <img src={instagramIcon} />
+          </a>
+          <a href="http://www.youtube.com/results?search_query=kalvhagen+sthlm&sm=3" target="_blank">
+            <img src={youtubeIcon} />
+          </a>
+        </div>
         <div className="address">
-            0709 351521
+            070 935 15 21
             Kalvhagen Stockholm
             Tulegatan 35
             113 53 Stockholm
