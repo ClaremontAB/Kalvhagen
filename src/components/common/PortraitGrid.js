a2236fb7919c0c2d4610dd4d705174321b7eb068
magnusharlin <magnusharlin>
2017-12-08 16:18:55 +0100

    added tsom statehandling to portraitGrid to determine whether the spcific image is flipped or not att the moment, could be used in a different way later

--- src/components/common/PortraitGrid.js
+++ src/components/common/PortraitGrid.js
@@ -1,52 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Image, Grid, Row, Col, Clearfix} from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const fillBlank = (idx) => {
-  let retObject;
-  if (idx % 4 === 0 && idx !== 0) {
-    retObject = <Clearfix visibleMdBlock visibleLgBlock visibleSmBlock/>;
-  } else if (idx % 2 === 0 && idx !== 0) {
-    retObject = <Clearfix visibleSmBlock/>;
+
+class PortraitGrid extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      portraits: props.portraits,
+    }
   }
+  fillBlank = (idx) => {
+    let retObject;
+    if (idx % 4 === 0 && idx !== 0) {
+      retObject = <Clearfix visibleMdBlock visibleLgBlock visibleSmBlock/>;
+    } else if (idx % 2 === 0 && idx !== 0) {
+      retObject = <Clearfix visibleSmBlock/>;
+    }
 
-  return retObject;
-};
+    return retObject;
+  };
 
-const imageClicked = (e, portrait) => {
-  e.preventDefault();
-  let target = e.target;
-  if (target.className !== "clicked-image") {
-      target.className = "clicked-image";
-  } else {
-    target.className = "image-container";
+  imageClicked = (idx) => {
+    let portraits = {...this.state.portraits};
+
+    portraits[idx].flip = portraits[idx].flip ? false : true;
+
+    this.setState({
+      portraits: portraits,
+    })
+  };
+
+  render () {
+    return (
+        <Grid style={{paddingTop: "20px"}}>
+          <Row className="show-grid">
+            {this.props.portraits.map((portrait, idx) =>
+              (<div key={idx}>
+                {this.fillBlank(idx)}
+                <Col sm={6} md={3} xsOffset={0} mdOffset={0} smOffset={0}>
+                  <div className='portrait-wrapper' onClick={() => this.imageClicked(idx)}>
+                    {portrait.flip ?
+                      (<div style={{width:'inherit', height:'auto', paddingBottom: '100%'}}>
+                        <Image src={portrait.imgSrc} responsive  className="image-container" />
+                        <div className='overlay' />
+                        <h4 className='portrait-name'> {portrait.name} </h4>
+                        <p className='text-box'> {portrait.desc} </p>
+                      </div>
+                    )
+                      : <Image src={portrait.imgSrc} responsive  className="image-container" />}
+                  </div>
+                  <div style={{marginRight: ""}}>
+                  </div>
+                </Col>
+              </div>)
+            )}
+          </Row>
+        </Grid>
+    );
   }
-  return portrait.src;
-};
-
-const PortraitGrid = ({portraits}) => {
-
-  return (
-      <Grid style={{paddingTop: "20px"}}>
-        <Row className="show-grid">
-          {portraits.map((portrait, idx) =>
-            (<div key={idx}>
-              {fillBlank(idx)}
-              <Col sm={6} md={3} xsOffset={0} mdOffset={0} smOffset={0}>
-                <div>
-                  <Image src='path' onClick={imageClicked, portrait} responsive  className="image-container" />
-                </div>
-                <div style={{marginRight: ""}}>
-                  <h4> {portrait.name} </h4>
-                  <p> {portrait.desc} </p>
-                </div>
-              </Col>
-            </div>)
-          )}
-        </Row>
-      </Grid>
-  );
-};
+}
 
 PortraitGrid.propTypes = {
   portraits: PropTypes.array.isRequired
